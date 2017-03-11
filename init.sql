7084893614301bb5ece2622804b864826f1cc302
Brice TEXIER <brice@ekylibre.com>
2017-03-11 10:02:36 +0100

    Fixes mysql migration

--- init.sql
+++ init.sql
@@ -4,11 +4,11 @@ CREATE TABLE IF NOT EXISTS reponses (
   x DECIMAL,
   y DECIMAL,
   reponse TEXT,
-  idsol VARCHAR,
-  nom_officiel VARCHAR,
-  nom_referentiel VARCHAR,
-  calcaire VARCHAR,
-  pierrosite VARCHAR,
-  texture VARCHAR,
-  hydromorphie VARCHAR
+  idsol VARCHAR(60),
+  nom_officiel VARCHAR(200),
+  nom_referentiel VARCHAR(200),
+  calcaire VARCHAR(60),
+  pierrosite VARCHAR(60),
+  texture VARCHAR(60),
+  hydromorphie VARCHAR(60)
 );
