3cc0b410001f0e19a48749a4898593bd4a03526a
Brice TEXIER <brice@ekylibre.com>
2017-03-13 21:29:44 +0100

    Adds DB switch for observation creation

--- api/observations/create.php
+++ api/observations/create.php
@@ -9,34 +9,33 @@ if ($_POST || $debug) {
         $raw_data = '{"id_utilisateur":"123457","x":"1,123456","y":"1,123456","reponse":"1","idsol":"ABC123","nom_officiel":"bla","nom_referentiel":"bla","calcaire":"bla","pierrosite":"bla","texture":"bla","hydromorphie":"bla"}';
 	else
         $raw_data = file_get_contents('php://input');
-    /*
-	$data = json_decode($raw_data, true);
-	$conn = new mysqli ($server, $user, $pass, $db); 
-	if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-	}
-	$id_utilisateur = $data["id_utilisateur"];
-	$x = $data["x"];
-	$y = $data["y"];
-	$reponse = $data["reponse"];
-	$idsol = $data["idsol"];
-	$nom_officiel = $data["nom_officiel"];
-	$nom_referentiel = $data["nom_referentiel"];
-	$calcaire = $data["calcaire"];
-	$pierrosite = $data["pierrosite"];
-	$texture= $data["texture"];
-	$hydromorphie= $data["hydromorphie"];
+    if ($db) {
+        $data = json_decode($raw_data, true);
+        $conn = new mysqli ($server, $user, $pass, $db); 
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $id_utilisateur = $data["id_utilisateur"];
+        $x = $data["x"];
+        $y = $data["y"];
+        $reponse = $data["reponse"];
+        $idsol = $data["idsol"];
+        $nom_officiel = $data["nom_officiel"];
+        $nom_referentiel = $data["nom_referentiel"];
+        $calcaire = $data["calcaire"];
+        $pierrosite = $data["pierrosite"];
+        $texture= $data["texture"];
+        $hydromorphie= $data["hydromorphie"];
 
-	$request = "INSERT INTO reponses (id_utilisateur, x, y, reponse, idsol, nom_officiel,nom_referentiel,calcaire,pierrosite,texture,hydromorphie) VALUES ('$id_utilisateur', '$x', '$y', '$reponse', '$idsol', '$nom_officiel', '$nom_referentiel','$calcaire','$pierrosite','$texture','$hydromorphie')";
-	if ($conn->query($request)) {
-        if ($debug)    echo "New record created successfully|";
-	} else {
-        if($debug)    echo "Error: " . $sql . "|" . $conn->error;
-	}
-	if ($debug)
-        $conn->close();
-    */
-    /* 'cledesol-retour-utilisateur' */
+        $request = "INSERT INTO reponses (id_utilisateur, x, y, reponse, idsol, nom_officiel,nom_referentiel,calcaire,pierrosite,texture,hydromorphie) VALUES ('$id_utilisateur', '$x', '$y', '$reponse', '$idsol', '$nom_officiel', '$nom_referentiel','$calcaire','$pierrosite','$texture','$hydromorphie')";
+        if ($conn->query($request)) {
+            if ($debug)    echo "New record created successfully|";
+        } else {
+            if($debug)    echo "Error: " . $sql . "|" . $conn->error;
+        }
+        if ($debug)
+            $conn->close();
+    }
     $options = array(
         'http' => array(
             'method' => 'POST',
